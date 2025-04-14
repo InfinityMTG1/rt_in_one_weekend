a84a69e80618373bc34645ec56d68fc058386514
Kieran Davies <attempt4032@protonmail.com>
2025-04-14 13:26:49 +0100

    Refactored some of the hit calculation math and introduced the interval class

--- src/sphere.h
+++ src/sphere.h
@@ -6,21 +6,21 @@
 class sphere: public hittable {
 public:
   sphere(point3 _center, double _radius): center(_center), radius(_radius) {}
-  bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+  bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
+    auto h = dot(r.direction(), oc);
     auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h- a*c;
 
-    auto discriminant = half_b*half_b - a*c;
     if (discriminant < 0) return false;
     auto sqrtd = sqrt(discriminant);
 
     // Find the nearest root that lies in the acceptable range.
-    auto root = (-half_b - sqrtd) / a;
-    if (root <= ray_tmin || ray_tmax <= root) {
-      root = (-half_b + sqrtd) / a;
-      if (root <= ray_tmin || ray_tmax <= root) return false;
+    auto root = (h- sqrtd) / a;
+    if (root <= ray_t.min || ray_t.max <= root) {
+      root = (h+ sqrtd) / a;
+      if (!ray_t.surrounds(root)) return false;
     }
 
     rec.t = root;
