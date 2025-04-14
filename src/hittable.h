4014922fa2077880c9925c555954a974f30f99aa
Kieran Davies <attempt4032@protonmail.com>
2025-04-14 13:24:32 +0100

    moved uses of t_min and t_max into a single interval class

--- src/hittable.h
+++ src/hittable.h
@@ -1,6 +1,9 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include "rtweekend.h"
+#include "vec3.h"
+
 class hit_record {
 
 public:
@@ -22,7 +25,7 @@ class hittable {
 public:
   virtual ~hittable() = default;
 
-  virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+  virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 
 };
 
